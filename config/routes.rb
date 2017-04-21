b0cdb91a7a90e652b3e628aea3d00cff087b1a3b
App Academy Student <student@appacademy.io>
2017-04-21 10:56:15 -0700

    Add controllers,models, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+  #
+  # get 'sessions/create'
+  #
+  # get 'sessions/destroy'
+
+  resource :sessions, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create, :edit, :update, :show]
+  # get 'users/new'
+  #
+  # get 'users/create'
+  #
+  # get 'users/edit'
+  #
+  # get 'users/update'
+  #
+  # get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
